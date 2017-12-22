3762984bcf48fd1143b5f0865de7aa5abcc43be9
adriaalcala <aalcala@hundredrooms.com>
2017-12-23 00:23:06 +0100

    change routes

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,9 @@
 from api.views import robots, selectors
-robots = ['GET', '/robots.txt', robots.robots]
+robots = ['GET', '/util-aligner/robots.txt', robots.robots]
 selectors = [
-    ['GET', '/database', selectors.get_databases],
-    ['GET', '/networks/{database}', selectors.get_networks],
-    ['GET', '/aligner', selectors.get_aligners],
+    ['GET', '/util-aligner/database', selectors.get_databases],
+    ['GET', '/util-aligner/networks/{database}', selectors.get_networks],
+    ['GET', '/util-aligner/aligner', selectors.get_aligners],
 ]
 
 
