9c302d3f6d7c9a82529c6146a4b6f6da3834bd4f
adriaalcala <aalcala@hundredrooms.com>
2017-12-23 01:58:57 +0100

    add route finished job

--- api/views/jobs.py
+++ api/views/jobs.py
@@ -2,6 +2,12 @@ from aiohttp.web import Response
 import json
 
 async def create_job(request):
+    data = await request.json()
     server_create_job(data)
     return Response(body='New job created',
                     status=200)
+
+async def finished_job(request):
+    data = await request.json()
+    server_finished_job(data)
+    return Response(status=200)
