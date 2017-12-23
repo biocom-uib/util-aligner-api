98267b12367199dbf38e89e1fc19cc5212872807
adriaalcala <aalcala@hundredrooms.com>
2017-12-23 01:55:23 +0100

    added jobs views

---
+++ api/views/jobs.py
@@ -0,0 +1,7 @@
+from aiohttp.web import Response
+import json
+
+async def create_job(request):
+    server_create_job(data)
+    return Response(body='New job created',
+                    status=200)
