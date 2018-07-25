a31c804f110ed50e390b64d0d993f188325a1323
bielr <gabriel.riera@uib.es>
2018-07-25 10:55:02 +0200

    stringdb

--- api/views/jobs.py
+++ api/views/jobs.py
@@ -1,9 +1,17 @@
 from aiohttp.web import Response
 from api.aligner import server_create_job, server_finished_job
 
+import re
+
+
+def fix_stringdb_species_id(data):
+    if 'db' in data and data['db'].lower() == 'stringdb':
+        data['net1'] = int(re.sub(r'^.*\(NCBI: (\d+)\)$', r'\1', data['net1']))
+        data['net2'] = int(re.sub(r'^.*\(NCBI: (\d+)\)$', r'\1', data['net2']))
 
 async def create_job(request):
     data = request.post_json
+    fix_stringdb_species_id(data)
     await server_create_job(request.app['master_pool'],
                             request.app['redis_pool'], request.app['celery'],
                             request.app['mongo_db'], data)
@@ -13,6 +21,7 @@ async def create_job(request):
 
 async def finished_job(request):
     data = request.post_json
+    fix_stringdb_species_id(data)
     await server_finished_job(request.app['mongo_db'],
                               request.app['redis_pool'], data['job_id'],
                               data['result_id'])
--- api/views/selectors.py
+++ api/views/selectors.py
@@ -7,7 +7,7 @@ settings = get_config()
 
 
 async def get_databases(request):
-    data = {'data': ['IsoBase']}
+    data = {'data': ['StringDB']}
     headers = settings.get('HEADERS')
     return Response(body=json.dumps(data),
                     content_type="application/json",
@@ -16,7 +16,21 @@ async def get_databases(request):
 
 
 async def get_networks(request):
-    data = {'data': ['cel', 'dme', 'sce', 'hsa', 'mus']}
+    mysql_conn = request.db
+
+    db_name = request.match_info['database']
+
+    if db_name.lower() == 'stringdb':
+        async with mysql_conn.cursor() as mysql_cursor:
+            await mysql_cursor.execute('select species_id, official_name from stringdb_species;')
+
+            species = [f'{official_name} (NCBI: {species_id})'
+                        for species_id, official_name in await mysql_cursor.fetchall()]
+
+        data = {'data': species}
+    else:
+        data = {'data': []}
+
     headers = settings.get('HEADERS')
     return Response(body=json.dumps(data),
                     content_type="application/json",
