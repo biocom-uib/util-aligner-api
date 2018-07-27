4184d0d4d89eba7305707adead8bbb09906e83e5
bielr <gabriel.riera@uib.es>
2018-07-27 10:59:09 +0200

    change csv processing

--- api/request/stringdb.py
+++ api/request/stringdb.py
@@ -1,4 +1,7 @@
+from csv import reader as csv_reader
+from io import StringIO
 import re
+
 from api.constants import PERSONALIZED_NETWORK
 
 
@@ -14,7 +17,7 @@ def get_species_id(net):
 def get_scores(thresholds, input_network=True):
     net = 'range_net_1_' if input_network else 'range_net_2_'
     scores = {
-        score[len(net):]: threshold
+        score[len(net):]: int(threshold)
         for score, threshold in thresholds.items()
         if score.startswith(net)
     }
@@ -23,10 +26,14 @@ def get_scores(thresholds, input_network=True):
 
 def get_network_data(data, input_network=True):
     net = 'net1' if input_network else 'net2'
-    edges = 'proteins_1' if input_network else 'proteins_2'
+    edges_key = 'proteins_1' if input_network else 'proteins_2'
+
+    edges = [(int(p1), int(p2)) for p1,p2 in csv_reader(StringIO(data.get(edges_key, "")), delimiter='\t')]
+
     data = {
         'species_id': get_species_id(data[net]),
         'score_thresholds': get_scores(data['scores_net'], input_network=input_network),
-        'edges': [edge.split(',') for edge in data.get(edges, [])]
+        'edges': edges
     }
+
     return data
