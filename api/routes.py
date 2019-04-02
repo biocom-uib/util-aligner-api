549cde89acfb5ed6656799925901e1634b02f105
bielr <gabriel.riera@uib.es>
2019-04-02 12:23:16 +0200

    add /alignment, /comparison and /status routes for job status polling and result retrieval

--- api/routes.py
+++ api/routes.py
@@ -6,17 +6,20 @@ BASE_PATH = config['BASE_PATH']
 robots = ['GET', '/util-aligner/robots.txt', v2.robots.robots]
 
 selectors = [
-    ['GET', f'{BASE_PATH}/v2/database',              v2.selectors.get_databases],
-    ['GET', f'{BASE_PATH}/v2/networks/{{database}}', v2.selectors.get_networks],
-    ['GET', f'{BASE_PATH}/v2/aligner',               v2.selectors.get_aligners],
+    ['GET', f'{BASE_PATH}/v2/database',                 v2.selectors.get_databases],
+    ['GET', f'{BASE_PATH}/v2/networks/{{database}}',    v2.selectors.get_networks],
+    ['GET', f'{BASE_PATH}/v2/aligner',                  v2.selectors.get_aligners],
 
-    ['GET', f'{BASE_PATH}/v2/file/{{filename}}',     v2.selectors.get_mongo_file],
+    ['GET', f'{BASE_PATH}/v2/file/{{filename}}',        v2.selectors.get_mongo_file],
+    ['GET', f'{BASE_PATH}/v2/alignment/{{result_id}}',  v2.selectors.get_mongo_alignment],
+    ['GET', f'{BASE_PATH}/v2/comparison/{{result_id}}', v2.selectors.get_mongo_comparison],
 ]
 
 jobs = [
-    ['POST', f'{BASE_PATH}/v2/create-job',          v2.jobs.create_job],
-    ['POST', f'{BASE_PATH}/v2/finished-alignment',  v2.jobs.finished_alignment],
-    ['POST', f'{BASE_PATH}/v2/finished-comparison', v2.jobs.finished_comparison],
+    ['GET',  f'{BASE_PATH}/v2/status',                  v2.jobs.status],
+    ['POST', f'{BASE_PATH}/v2/create-job',              v2.jobs.create_job],
+    ['POST', f'{BASE_PATH}/v2/finished-alignment',      v2.jobs.finished_alignment],
+    ['POST', f'{BASE_PATH}/v2/finished-comparison',     v2.jobs.finished_comparison],
 ]
 
 routes = [robots] + selectors + jobs
--- api/v2/views/jobs.py
+++ api/v2/views/jobs.py
@@ -1,7 +1,7 @@
 from aiohttp.web import Response
 import json
 
-from api.aligner import server_create_alignment_job_group, server_finished_alignment, server_finished_comparison
+import api.aligner as aligner
 from api.v2.request import adapt_data
 
 from config import get_config
@@ -12,7 +12,7 @@ settings = get_config()
 async def create_job(request):
     data = request.post_json
     data = adapt_data(data)
-    job_id = await server_create_alignment_job_group(
+    job_id = await aligner.server_create_alignment_job_group(
             request.app['redis_pool'], request.app['celery'],
             request.app['mongo_db'], request.app['mongo_gridfs'], data)
 
@@ -23,9 +23,29 @@ async def create_job(request):
                     status=200)
 
 
+async def status(request):
+    job_id = request.rel_url.query['job_id']
+    cache_connection = request.app['redis_pool']
+
+    result = await aligner.get_job_result_id(cache_connection, job_id)
+
+    if result is not None:
+        resp_data = {'result': result}
+
+    else:
+        group_status = await aligner.get_job_group_status(cache_connection, job_id)
+        resp_data = group_status if group_status['njobs'] > 0 else {}
+
+    headers = settings.get('HEADERS')
+    return Response(body=json.dumps(resp_data),
+                    content_type='application/json',
+                    headers=headers,
+                    status=200)
+
+
 async def finished_alignment(request):
     data = request.post_json
-    await server_finished_alignment(request.app['mongo_db'], request.app['mongo_gridfs'],
+    await aligner.server_finished_alignment(request.app['mongo_db'], request.app['mongo_gridfs'],
                               request.app['redis_pool'], request.app['celery'],
                               data['job_id'], data['result_id'])
     return Response(status=200)
@@ -33,7 +53,7 @@ async def finished_alignment(request):
 
 async def finished_comparison(request):
     data = request.post_json
-    await server_finished_comparison(request.app['mongo_db'], request.app['mongo_gridfs'],
+    await aligner.server_finished_comparison(request.app['mongo_db'], request.app['mongo_gridfs'],
                                        request.app['redis_pool'], data['job_id'],
                                        data['result_id'])
     return Response(status=200)
